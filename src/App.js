220627966153195de0e34f20df73069b743d614d
Jolidog <jolidog@gmail.com>
2017-10-27 20:28:22 +0100

    Added data sources.

--- src/App.js
+++ src/App.js
@@ -2,63 +2,70 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Whatshot from 'material-ui-icons/Whatshot';
+
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
 class App extends Component {
+
+  renderTimelineItems(details) {
+    return details.map((entry) => {
+      return (
+        <VerticalTimelineElement
+          key={entry.key}
+          title={entry.title}
+          date={entry.date}
+          subtitle={entry.subtitle}
+          content={entry.content}
+          iconStyle={{
+            background: "#f00",
+            color: "#fff"
+          }}
+          icon={<Whatshot />}
+        />
+      );
+    });
+  } 
+
+  renderTimeline(data) {
+    // Check if a render function exists for the paragraph type and call it
+    if (!data.details) {
+      return null;
+    }
+
+    return (
+      <VerticalTimeline>
+        {this.renderTimelineItems(data.details)}
+      </VerticalTimeline>
+    );
+  }
+
   render() {
+    function getUrlQueryParameterByName(name, url) {
+      if (!url) url = window.location.href;
+      name = name.replace(/[[\]]/g, "\\$&");
+      const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+      const results = regex.exec(url);
+      if (!results) return 'default';
+      if (!results[2]) return 'default';
+      return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+
+    const jsonName = getUrlQueryParameterByName('event');
+    const data = require('./data/' + jsonName + '.json');
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{data.event}</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {data.description}
         </p>
 
-        <VerticalTimeline>
-            <VerticalTimelineElement
-              key="1"
-              title="Creative Director"
-              subtitle="Miami, FL"
-              content="Creative Direction, User Experience, Visual Design, Project Management, Team Leading"
-              date="2011 - present"
-              iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            />
-            <VerticalTimelineElement
-              key="2"
-              title="Art Director"
-              subtitle="San Francisco, CA"
-              content="Creative Direction, User Experience, Visual Design, SEO, Online Marketing"
-              date="2006 - 2011"
-              iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            />
-            <VerticalTimelineElement
-              key="3"
-              title="Content Marketing for Web, Mobile and Social Media"
-              subtitle="Online Course"
-              content="Strategy, Social Media"
-              date="April 2013"
-              iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-            />
-            <VerticalTimelineElement
-              key="4"
-              title="Agile Development Scrum Master"
-              subtitle="Certification"
-              content="Creative Direction, User Experience, Visual Design"
-              date="November 2012"
-              iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-            />
-            <VerticalTimelineElement
-              key="5"
-              title="Bachelor of Science in Interactive Digital Media Visual Imaging"
-              subtitle="Bachelor Degree"
-              content="Creative Direction, Visual Design"
-              date="2002 - 2006"
-              iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-            />
-        </VerticalTimeline>
+        {this.renderTimeline(data)}
       </div>
     );
   }
